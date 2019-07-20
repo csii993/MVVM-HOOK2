a2633dc941b36ac6243fecc8c336bc3b0d7508d7
wxd24 <csiivip@163.com>
2019-07-20 17:58:01 +0800

    Initial Commit

---
+++ MVVM-HOOK2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVM-HOOK2
+//
+//  Created by wangxiaodong on 20/07/2019.
+//  Copyright © 2019 WXD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MVVM-HOOK2/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MVVM-HOOK2
+//
+//  Created by wangxiaodong on 20/07/2019.
+//  Copyright © 2019 WXD. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
